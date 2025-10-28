a2ad118805aaa3ff0ac41c25dfff0aa63884104f
shagelund <s132347@dtu.dk>
2025-10-28 11:07:50 +0100

    Added some more text to it /s132347

--- R/functionOne.R
+++ R/functionOne.R
@@ -1,4 +1,9 @@
-#' Codons
+#' Generates DNA sequence from sample
+#'
+#' This function first allows a dna size like e.g. 9 nucleotides,
+#' and allows them to be repeated. In the end it merges them together which is
+#' what collapse do. In the end you shoud get something like AATGCCGTA if
+#' you have a sample size of 9.
 #'
 #' @param dnaSize
 #'

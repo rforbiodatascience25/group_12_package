8d8d4ab2d312d7f2846c0610d68c340da01ca6b9
teranni <heszteranna@gmail.com>
2025-10-28 10:00:29 +0100

    Codon Table

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Title: Codon Table
+#'
+#' Table of codons with their coded amino acids.
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codonTable"

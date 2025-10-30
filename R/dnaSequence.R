e2aadd3b84f284e87b33522d77d438cb2c978c6e
shagelund <s132347@dtu.dk>
2025-10-30 10:46:18 +0100

    Added function 1 and updated som text/s132347 (sofie)

---
+++ R/dnaSequence.R
@@ -0,0 +1,18 @@
+#' Generates dna sequence from sample
+#'
+#' This function generates a random DNA sequence of a specified length.
+#' It samples nucleotides ("A", "T", "G", "C") with replacement, meaning
+#' bases can repeat, and concatenates them into a single string.
+#' For example, with a sequence size of 9, the output could be "AATGCCGTA".
+#'
+#' @param dnaSize Integer. The desired length of the DNA sequence.
+#'
+#' @returns DNA - A character string representing a DNA sequence.
+#' @export
+#'
+#' @examples
+dnaSequence <- function(dnaSize){
+  dnaTmp <- sample(c("A", "T", "G", "C"), size = dnaSize, replace = TRUE)
+  dna <- paste0(dnaTmp, collapse = "")
+  return(dna)
+}
